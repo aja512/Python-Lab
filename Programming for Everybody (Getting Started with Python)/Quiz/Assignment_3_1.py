4bdf943ce3db3353730069ae085d714a6c92c9a4
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2021-11-28 22:33:17 +0530

    Create Assignment_3_1.py

---
+++ Programming for Everybody (Getting Started with Python)/Quiz/Assignment_3_1.py
@@ -0,0 +1,19 @@
+"""
+3.1 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking the user input - assume the user types numbers properly.
+"""
+
+def computepay(h,r):
+    if h < 0 or r < 0:
+        return None
+    elif h > 40:
+        return (40*r+(h-40)*1.5*r)
+    else:
+        return (h*r)
+    
+try:
+    hour = float(input("Enter Hours:"))
+    rate = float(input("please input your rate:"))
+    p = computepay(hour,rate)
+    print(p)
+except:
+    print("Please,input your numberic")
