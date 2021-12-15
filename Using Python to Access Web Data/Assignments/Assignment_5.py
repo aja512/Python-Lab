35bca0d67c667fcdce2f3af1d4e98fab76520c50
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2021-12-16 00:09:11 +0530

    Add files via upload

---
+++ Using Python to Access Web Data/Assignments/Assignment_5.py
@@ -0,0 +1,20 @@
+import urllib.request as ur
+import xml.etree.ElementTree as et
+
+url = input('Enter Location: ')
+
+total_number = 0
+sum = 0
+
+print('Retrieving', url)
+xml = ur.urlopen(url).read()
+print('Retrieved', len(xml), 'characters')
+
+tree = et.fromstring(xml)
+counts = tree.findall('.//count')
+for count in counts:
+    sum += int(count.text)
+    total_number += 1
+
+print('Count:', total_number)
+print('Sum:', sum)
