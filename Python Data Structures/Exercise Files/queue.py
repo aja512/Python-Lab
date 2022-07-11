cde7490a6deaa8f318179f09266798acaa14b362
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2022-07-11 20:04:15 +0530

    Create queue.py

---
+++ Python Data Structures/Exercise Files/queue.py
@@ -0,0 +1,23 @@
+# Initializing a queue
+queue = []
+
+# Adding elements to the queue
+queue.append('g')
+queue.append('f')
+queue.append('g')
+
+print("Initial queue")
+print(queue)
+
+# Removing elements from the queue
+print("\nElements dequeued from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing elements")
+print(queue)
+
+# Uncommenting print(queue.pop(0))
+# will raise and IndexError
+# as the queue is now empty
